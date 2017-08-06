b04e0922ff8b329ddc3d9f9771ca01130776d509
Juntao Han <499445428@qq.com>
2017-08-06 13:25:59 +0800

    refine code style

--- index.php
+++ index.php
@@ -14,17 +14,17 @@
  * 首页入口
  +----------------------------------
  */
-//将目前thinkphp生产模式变为开发模式
+// 将目前thinkphp生产模式变为开发模式
 define('APP_DEBUG', TRUE);
 
-//定义项目路径
+// 定义项目路径
 define('APP_PATH', './Application/');
 
-//css,js,images总路径
+// css,js,images总路径
 define('SOURCE_URL', "./Public/");
 
-//定义上传文件位置
+// 定义上传文件位置
 define('UPLOAD_URL',"./Uploads/");
 
-//引入thinkphp核心程序
+// 引入thinkphp核心程序
 require './ThinkPHP/ThinkPHP.php';
